cc5482ef3f66b53542c1efb932dd285bbbb5c355
unknown <rayaneay04@gmail.com>
2026-02-02 16:52:25 +0100

    first commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 
 # =====================================================
 # CONFIGURATION DE LA PAGE
-# =====================================================
+# ====================================================
 st.set_page_config(
     page_title="Plateforme d'Optimisation des Examens",
     page_icon="",
